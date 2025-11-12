70b87fa8acf8a286a1a96efad199930aaa60dcf6
linzhuiqi <linzq59@chinaunicom.cn>
2025-11-12 17:35:18 +0800

    refactor(web): 把statistics组件放到permission下

--- web/src/api/permission/statistic.js
+++ web/src/api/permission/statistic.js
@@ -1,9 +1,10 @@
 import service from "@/utils/request"
+const BASE_URL = '/user/api/v1'
 
 // 获取客户端统计数据
 export const getData = (params) => {
     return service({
-        url: "/user/api/v1/statistic/client",
+        url: `${BASE_URL}/statistic/client"`,
         method: "get",
         params,
     });
